4176806deec06ef43a01e16fda1d23b461fa057a
Collin Edwards <collinedwards1579@gmail.com>
2018-01-18 22:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log.toUpperCase(string)
+}
+
+function logWhisper(string){
+  console.log.toLowerCase(string)
+}

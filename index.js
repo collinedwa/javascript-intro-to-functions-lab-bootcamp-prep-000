8bf3fc6175c4ec780df95256f2aad92ba1a9ae6a
Collin Edwards <collinedwards1579@gmail.com>
2018-01-18 22:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(toUpperCase(string))
+  console.log(string).toUpperCase
 }
 
 function logWhisper(string){
-  console.log(toLowerCase(string))
+  console.log(string).toLowerCase
 }
 
 function sayHiToGrandma(string){

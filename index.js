6a50990eacf9cf9ad807cd730bf1cfffbeb394e3
Collin Edwards <collinedwards1579@gmail.com>
2018-01-18 23:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return string
+  if(string === string.toLowerCase())
+    return "I can't hear you!"
+  else if(string === string.toUpperCase())
+    return "YES INDEED!"
+  else
+    return "I love you, too."
 }
 
-sayHiToGrandma(whisper)
+sayHiToGrandma(string.toLowerCase)
 
-sayHiToGrandma(shout)
+sayHiToGrandma(string.toUpperCase)
+
+sayHiToGrandma()
